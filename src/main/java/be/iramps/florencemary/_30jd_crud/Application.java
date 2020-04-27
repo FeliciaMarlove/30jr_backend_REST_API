c6a0c3add4a05fde523aa3a679c6d8624db06e43
FeliciaMarlove <missflorencemary@hotmail.com>
2020-04-27 17:42:09 +0200

    Initial commit, empty project

---
+++ src/main/java/be/iramps/florencemary/_30jd_crud/Application.java
@@ -0,0 +1,13 @@
+package be.iramps.florencemary._30jd_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/be/iramps/florencemary/_30jd_crud/ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.iramps.florencemary._30jd_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
