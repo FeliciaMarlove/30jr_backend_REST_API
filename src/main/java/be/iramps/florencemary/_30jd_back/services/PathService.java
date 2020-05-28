5afd2675260a53ad078444cc327690786e93d544
FeliciaMarlove <missflorencemary@hotmail.com>
2020-05-28 20:00:37 +0200

    Adding tasks in paths altered to follow new data model

--- src/main/java/be/iramps/florencemary/_30jd_back/services/PathService.java
+++ src/main/java/be/iramps/florencemary/_30jd_back/services/PathService.java
@@ -41,45 +41,42 @@ public class PathService implements CRUDService {
     public Message addTask(Integer pathId, Integer taskId, Integer index) {
         int size = listTasks(pathId).size();
         Optional<Path> optionalPath = pathRepository.findById(pathId);
-        if (optionalPath.isPresent() &&  size < 30) {
+        if (optionalPath.isPresent() && size < 30) {
             Path p = optionalPath.get();
             Optional<Task> optionalTask = taskRepository.findById(taskId);
             if (optionalTask.isPresent() && optionalTask.get().isTaskActive() && !isAlreadyInPath(p, optionalTask.get())) {
                 Task task = optionalTask.get();
-                // index "-1" means "no index was specified"
-                if (index == -1) {
+                // index "-99" means "no index was specified"
+                if (index == -99) {
                     if (taskPathRepository.findByPath(p).isEmpty()) {
                         TaskPath tp = new TaskPath(task, p, 0);
                         taskPathRepository.save(tp);
                         return new Message("Défi ajouté", true);
                     }
                     List<TaskPath> all = taskPathRepository.findByPathOrderByPosition(p);
-                    Integer higherPosition = all.get(all.size()-1).getPosition();
-                    TaskPath tp = new TaskPath(task, p, higherPosition+1);
+                    Integer higherPosition = all.get(all.size() - 1).getPosition();
+                    TaskPath tp = new TaskPath(task, p, higherPosition + 1);
                     taskPathRepository.save(tp);
                     return new Message("Défi ajouté", true);
+                    // if index was specified
                 } else {
-                    if (index >= 0 &&
-                            (index <= size
-                                    ||
-                                    (listTasks(pathId).isEmpty() && index == 0))
-                    ) {
-                        Optional<TaskPath> optionalTaskPath = taskPathRepository.findByPathAndTask(p, task);
-                        if (optionalTaskPath.isPresent()) {
-                            TaskPath tp = optionalTaskPath.get();
-                            for(TaskPath each: taskPathRepository.findAll()) {
-                                if (each.getPath().getPathId().equals(pathId)
-                                && each.getPosition() >= index) {
-                                    each.setPosition(each.getPosition() + 1);
-                                }
-                            }
-                            tp.setPosition(index);
-                            taskPathRepository.save(tp);
-                        }
+                    if (listTasks(pathId).isEmpty() && index == 0) {
+                        TaskPath tp = new TaskPath(task, p, 0);
+                        taskPathRepository.save(tp);
                         return new Message("Défi ajouté", true);
-                    } else {
-                        return new Message("L'index n'est pas valide", false);
                     }
+                    if (index > 0 && index <= size) {
+                        List<TaskPath> all = taskPathRepository.findByPath(p).get();
+                        for (TaskPath each : all) {
+                            if (each.getPosition() >= index) {
+                                each.setPosition(each.getPosition() + 1);
+                            }
+                        }
+                        TaskPath tp = new TaskPath(task, p, index);
+                        taskPathRepository.save(tp);
+                        return new Message("Défi ajouté", true);
+                    }
+                    return new Message("L'index n'est pas valide", false);
                 }
             }
             return new Message("Le défi " + optionalTask.get().getTaskName() + " est déjà dans la liste", false);
@@ -88,8 +85,8 @@ public class PathService implements CRUDService {
     }
 
     private boolean isAlreadyInPath(Path path, Task task) {
-        for(DTOEntity t: listTasks(path.getPathId())) {
-            if (((TaskGet)(t)).getTaskId().equals(task.getTaskId())) return true;
+        for (DTOEntity t : listTasks(path.getPathId())) {
+            if (((TaskGet) (t)).getTaskId().equals(task.getTaskId())) return true;
         }
         return false;
     }
@@ -120,14 +117,14 @@ public class PathService implements CRUDService {
         List<TaskPath> tpList = new ArrayList<>();
         Optional<Path> optionalPath = pathRepository.findById(id);
         if (optionalPath.isPresent()) {
-            for (TaskPath tp: taskPathRepository.findAll()) {
+            for (TaskPath tp : taskPathRepository.findAll()) {
                 if (tp.getPath().getPathId().equals(id)) {
                     tpList.add(tp);
                 }
             }
         }
         Collections.sort(tpList);
-        for (TaskPath tp: tpList) {
+        for (TaskPath tp : tpList) {
             list.add(new DtoUtils().convertToDto(tp.getTask(), new TaskGet()));
         }
         return list;
@@ -137,20 +134,20 @@ public class PathService implements CRUDService {
     public DTOEntity read(Integer id) {
         Optional<Path> optPath = pathRepository.findById(id);
         return optPath.isPresent() ?
-                new DtoUtils().convertToDto(optPath.get(), new PathGet()) : new Message("Le parcours n'a pas été trouvé", false) ;
+                new DtoUtils().convertToDto(optPath.get(), new PathGet()) : new Message("Le parcours n'a pas été trouvé", false);
     }
 
     @Override
     public List<DTOEntity> read() {
         List<DTOEntity> list = new ArrayList<>();
-        for(Path p: pathRepository.findAll()) list.add(new DtoUtils().convertToDto(p, new PathGet()));
+        for (Path p : pathRepository.findAll()) list.add(new DtoUtils().convertToDto(p, new PathGet()));
         return list;
     }
 
     @Override
     public DTOEntity create(DTOEntity dtoEntity) {
-        if (pathRepository.findByPathName(((PathPost)dtoEntity).getPathName()) == null) {
-            Path p = (Path)new DtoUtils().convertToEntity(new Path(), dtoEntity);
+        if (pathRepository.findByPathName(((PathPost) dtoEntity).getPathName()) == null) {
+            Path p = (Path) new DtoUtils().convertToEntity(new Path(), dtoEntity);
             try {
                 pathRepository.save(p);
                 return new DtoUtils().convertToDto(p, new PathGet());
@@ -163,12 +160,12 @@ public class PathService implements CRUDService {
 
     @Override
     public DTOEntity update(Integer id, DTOEntity dtoEntity) {
-        if(pathRepository.existsById(id)) {
+        if (pathRepository.existsById(id)) {
             Path p = pathRepository.findById(id).get();
             try {
-                p.setPathName(((PathPost)dtoEntity).getPathName());
-                p.setPathShortDescription(((PathPost)dtoEntity).getPathShortDescription());
-                p.setPathLongDescription(((PathPost)dtoEntity).getPathLongDescription());
+                p.setPathName(((PathPost) dtoEntity).getPathName());
+                p.setPathShortDescription(((PathPost) dtoEntity).getPathShortDescription());
+                p.setPathLongDescription(((PathPost) dtoEntity).getPathLongDescription());
                 pathRepository.save(p);
                 return new DtoUtils().convertToDto(p, new PathGet());
             } catch (Exception e) {
@@ -182,7 +179,7 @@ public class PathService implements CRUDService {
     @Override
     public DTOEntity delete(Integer id) {
         Optional<Path> optPath = pathRepository.findById(id);
-        if(optPath.isPresent() && !pathIsUsed(id)) {
+        if (optPath.isPresent() && !pathIsUsed(id)) {
             optPath.get().setPathActive(false);
             pathRepository.save(optPath.get());
             return new DtoUtils().convertToDto(optPath.get(), new PathGet());
@@ -192,7 +189,7 @@ public class PathService implements CRUDService {
 
     public DTOEntity activate(Integer id) {
         Optional<Path> optPath = pathRepository.findById(id);
-        if(optPath.isPresent()) {
+        if (optPath.isPresent()) {
             optPath.get().setPathActive(true);
             pathRepository.save(optPath.get());
             return new DtoUtils().convertToDto(optPath.get(), new PathGet());
@@ -201,7 +198,7 @@ public class PathService implements CRUDService {
     }
 
     private boolean pathIsUsed(Integer id) {
-        for(UserPath up: userPathRepository.findAll()) {
+        for (UserPath up : userPathRepository.findAll()) {
             if ((up.getPath().getPathId().equals(id) && up.isOngoing())) return true;
         }
         return false;
